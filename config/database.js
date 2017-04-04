3b24addfeb51878956323e87ef15398a8e636d82
TaniaSolo <solonets.tatyana@gmail.como>
2017-04-04 01:50:30 -0700

    Add project.

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '68.178.143.94',
+        'user': 'TaniaS559789',
+        'password': 'house123456Z!'
+    },
+	'database': 'TaniaS559789',
+    'users_table': 'user'
+};

2dc79195b81aceb626728e9c4363bacb6d91fd65
TaniaSolo <solonets.tatyana@gmail.como>
2017-04-08 20:26:55 -0700

    Mongo db.

--- app.js
+++ app.js
@@ -1,38 +1,53 @@
 
-var express  = require('express');
-var session  = require('express-session');
-var cookieParser = require('cookie-parser');
+/**
+	* Node.js Login Boilerplate
+	* More Info : http://kitchen.braitsch.io/building-a-login-system-in-node-js-and-mongodb/
+	* Copyright (c) 2013-2016 Stephen Braitsch
+**/
+
+var http = require('http');
+var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var hbs = require('hbs');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var errorHandler = require('errorhandler');
+var cookieParser = require('cookie-parser');
+var MongoStore = require('connect-mongo')(session);
 
-var passport = require('passport');
-var flash    = require('connect-flash');
+var app = express();
 
-require('./config/passport')(passport);
-
-app.use(morgan('dev'));
+app.locals.pretty = true;
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'jade');
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+app.use(express.static(__dirname + '/app/public'));
 
-app.set('view engine', 'html');
-app.engine('html', hbs.__express);
+// build mongo database connection url //
+
+var dbHost = process.env.DB_HOST || 'localhost'
+var dbPort = process.env.DB_PORT || 27017;
+var dbName = process.env.DB_NAME || 'node-login';
+
+var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+if (app.get('env') == 'live'){
+// prepend url with authentication credentials // 
+	dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
+}
 
 app.use(session({
-	secret: 'vidyapathaisalwaysrunning',
+	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+	proxy: true,
 	resave: true,
-	saveUninitialized: true
- } ));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+	saveUninitialized: true,
+	store: new MongoStore({ url: dbURL })
+	})
+);
 
-require('./app/routes.js')(app, passport);
+require('./app/server/routes')(app);
 
-app.listen(port);
-console.log('app is start in ' + port);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
